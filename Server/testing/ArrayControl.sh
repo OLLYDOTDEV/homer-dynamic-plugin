3663efdda4e07ca1b55b451cca197be353a0e16a
OLLYDOTDEV <OLLYDOTDEV@gmail.com>
2021-08-20 06:32:08 +0000

    updated Array Read Function

--- Server/testing/ArrayControl.sh
+++ Server/testing/ArrayControl.sh
@@ -21,15 +21,22 @@ do
 
 #echo $element
 
-echo ${Ip_Address[$element]} ${Ports[$element]} ${Hostname[$element]} >>  $database_file
+echo ${Ip_Address[$element]} ${Ports[$element]} ${Hostname[$element]} >>  $database_file  
 done
 
 }
 
 Array_Read () { # used to read array from file
 
-mapfile dataarray < $database_file
-echo ${dataarray[@]}
+#mapfile dataarray < $database_file
+Ip_Address=($(awk '{print $1}' $database_file))
+Port=($(awk '{print $2}' $database_file))
+Hostname=($(awk '{print $3}' $database_file))
+
+echo ${Ip_Address[1]}
+echo ${Port[1]}
+echo ${Hostname[1]}
+
 
 
 }
@@ -53,7 +60,7 @@ return $in # returned valued of 0 = true
 somevar="192.168.1.5"
 
 list=("192.168.1.1" "192.168.1.2" "192.168.1.3")
-Array_Contains list "$somevar"  && match=true || match=false
+#Array_Contains list "$somevar"  && match=true || match=false
 
 if $match
 then 
@@ -62,7 +69,7 @@ else
  echo "No Match"
 fi
 
-#Array_Read
+Array_Read
 #Array_Write
 
 
